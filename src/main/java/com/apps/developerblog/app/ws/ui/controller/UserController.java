4aa9e226c7618c908cc5bcaf63cf2c7f45f30a0c
binuri <binurim95@gmail.com>
2024-07-27 15:57:29 +0530

    Validating HTTP POST request body

--- src/main/java/com/apps/developerblog/app/ws/ui/controller/UserController.java
+++ src/main/java/com/apps/developerblog/app/ws/ui/controller/UserController.java
@@ -2,6 +2,7 @@ package com.apps.developerblog.app.ws.ui.controller;
 
 import com.apps.developerblog.app.ws.ui.model.request.UserDetailsRequestModel;
 import com.apps.developerblog.app.ws.ui.model.response.UserRest;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -43,7 +44,7 @@ public class UserController  {
             produces = {
             MediaType.APPLICATION_XML_VALUE,
             MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<UserRest> createUser(@RequestBody UserDetailsRequestModel userDetails){
+    public ResponseEntity<UserRest> createUser(@Valid @RequestBody UserDetailsRequestModel userDetails){
         UserRest returnValue = new UserRest();
         returnValue.setEmail(userDetails.getEmail());
         returnValue.setFirstName(userDetails.getFirstName());
